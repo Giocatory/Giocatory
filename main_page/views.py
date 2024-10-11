af4a858db2950ee607273040d797a3f359c75946
Mikhail <myu.derkunov@udtech.ru>
2024-10-11 11:22:35 +0300

    набросок главной страницы

--- main_page/views.py
+++ main_page/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def main_page(request):
     context = {
-        'title': 'Giocatory - main_page',
+        'title': 'Giocatory - main page',
     }
 
     return render(request, 'main_page/index.html', context=context)
